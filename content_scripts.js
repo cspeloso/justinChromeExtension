00da03a1809920318eddcb2d8b11fcdac59a8c45
cspeloso <cspeloso@email.neit.edu>
2021-12-03 12:59:02 -0500

    Initial manifest V3 changes. updated manifest file and some JS

--- content_scripts.js
+++ content_scripts.js
@@ -1,12 +1,17 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)    {
     console.log(request.command);
 
+	ReplaceAllImagesWithJustin();
+	
+    sendResponse({result: "success"});
+});
+
+function ReplaceAllImagesWithJustin(){
 	var imgsArr = document.getElementsByTagName("img");
 	
 	for(var i = 0; i < imgsArr.length;i++)
 	{
 		imgsArr[i].src = chrome.runtime.getURL("images/justin.jpeg");
+		imgsArr[i].classList.add('justin-plante');
 	}
-	
-    sendResponse({result: "success"});
-});
+}
